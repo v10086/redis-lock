ae96c39e7977b85a64b8f4de86ccb48a9a98b8e0
v10086 <1146625863@qq.com>
2023-03-06 17:28:19 +0800

    Update RedisLock.php

--- src/RedisLock.php
+++ src/RedisLock.php
@@ -15,5 +15,15 @@ class RedisLock{
     //解锁
     public static function unlock($key){
         return self::$redisHandler->DEL('redLock:'.$key);
+        
+        //执行lua脚本 
+        return self::$redisHandler->EVAL('
+            if redis.call("GET", KEYS[1]) == ARGV[1] then
+                return redis.call("DEL", KEYS[1])
+            else
+                return 0
+            end
+        ', ['redLock:'.$key, 1], 1);
+        
     }
 }
